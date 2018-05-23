4feb89f7ec4454f91a9ae3e0faa903cd4cb41f69
WaximeA <aveline.maxime@gmail.com>
2018-05-23 11:12:19 +0200

    #wax-design: Display custom logo in header with helper methods

--- app/code/local/Wax/Design/Helper/Data.php
+++ app/code/local/Wax/Design/Helper/Data.php
@@ -2,5 +2,41 @@
 
 class Wax_Design_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * @var string CONFIG_ADDITIONAL_LOGO
+     */
+    const CONFIG_ADDITIONAL_LOGO = 'wax_design/custom_fields/additional_logo';
 
+    /**
+     * @var string FILE_PATH_MEDIA_DIRECTORY
+     */
+    const FILE_PATH_MEDIA_DIRECTORY = 'wax_design_logo';
+
+    /**
+     * Retrive custom field additional logo file name
+     *
+     * @return void
+     */
+    public function getAdditionalLogoFileName()
+    {
+        /** @var string $fileName */
+        $fileName = Mage::getStoreConfig(self::CONFIG_ADDITIONAL_LOGO);
+
+        return $fileName;
+    }
+
+    /**
+     * Get file path
+     *
+     * @param $fileName
+     *
+     * @return string
+     */
+    public function getFilePath($fileName)
+    {
+        /** @var string $filePath */
+        $filePath = Mage::getBaseUrl('media') . self::FILE_PATH_MEDIA_DIRECTORY . DS . $fileName;
+
+        return $filePath;
+    }
 }
